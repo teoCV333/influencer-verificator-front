bfb7571ae8f97b492a2dc10618fd456ab8faed63
MCVdev <mateocarvajalvergara333@gmail.com>
2025-01-12 04:29:14 -0500

    search page initial design

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { SearchComponent } from './pages/search/search.component';
+import { ProfileComponent } from './pages/profile/profile.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'dashboard', component: DashboardComponent },
+    { path: 'search', component: SearchComponent },
+    { path: 'profile', component: ProfileComponent },
+    { path: '', redirectTo: '/dashboard', pathMatch: 'full' }
+];
